1baaf86461b1d352f2320bb609bba31169a2890f
envizar <silent.gate@ya.ru>
2025-01-18 16:03:52 +0300

    trying to fix env variables

--- server/plugins/database.ts
+++ server/plugins/database.ts
@@ -4,13 +4,13 @@ export default defineNitroPlugin(() => {
     const config = useRuntimeConfig()
     const mongoURI = config.MONGODB_URI
 
-    if (mongoURI === undefined) {
+    if (mongoURI === undefined || mongoURI === "") {
         console.error("MongoDB connection string not loaded")
         return
     }
 
     if (!mongoose.connection.readyState) {
-        console.log(mongoURI)
+        console.log("uri: " + mongoURI)
         mongoose.connect(mongoURI).then(() => {
             console.log('Connected to MongoDB')
         }).catch(() => {
