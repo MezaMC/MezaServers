d980eb6539d72455ad216b4f71fc52c9e979c00f
envizar <silent.gate@ya.ru>
2025-01-18 16:11:44 +0300

    trying to fix env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import 'dotenv/config';
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
